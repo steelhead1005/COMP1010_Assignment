8d21a29a3d420c0c4ff9570d8bd827d38b24122d
Gabe Steel <steelhead1005@gmail.com>
2025-05-13 17:06:11 +1000

    Hard coded the user character creation (all of this will need to be changed)

--- src/Client.java
+++ src/Client.java
@@ -1,8 +1,35 @@
+import java.util.Scanner;
 public class Client {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        System.out.println("Game");
-        Character testChar = new Character("Test", 100, 0, 9, 10, 10);
+        Scanner scanner = new Scanner(System.in);
+        Character testChar = new Character(null, 0, 0, 0, 0, 0);
+        System.out.println("Whats your characters name: ");
+        testChar.name = scanner.nextLine();
+        System.out.print("Press ENTER to roll for health: ");
+        scanner.nextLine();
+        int rolledHealth = (int)(Math.random() * 101);
+        testChar.health = rolledHealth;
+        System.out.println("You rolled: " + rolledHealth);
+        System.out.print("Press ENTER to roll for strength: ");
+        scanner.nextLine();
+        int rolledStrength = (int)(Math.random() * 101);
+        testChar.strength = rolledStrength;
+        System.out.println("You rolled: " + rolledStrength);
+        System.out.print("Press ENTER to roll for intelligence: ");
+        scanner.nextLine();
+        int rolledInt = (int)(Math.random() * 101);
+        testChar.intelligence = rolledInt;
+        System.out.println("You rolled: " + rolledInt);
+        System.out.print("Press ENTER to roll for dexterity: ");
+        scanner.nextLine();
+        int rolledDex = (int)(Math.random() * 101);
+        testChar.dexterity = rolledDex;
+        System.out.println("You rolled: " + rolledDex);
+        System.out.print("Press ENTER to roll for defence: ");
+        scanner.nextLine();
+        int rolledDefence = (int)(Math.random() * 101);
+        testChar.defence = rolledDefence;
+        System.out.println("You rolled: " + rolledDefence);
         System.out.println(testChar);
     }
 }
