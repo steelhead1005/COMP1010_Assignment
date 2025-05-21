81e7cadd4643631c81c6486a870bc287f28e47d5
Gabe Steel <steelhead1005@gmail.com>
2025-05-21 18:43:19 +1000

    moved Returnpair into Character file

--- src/COMP1010/main/Character.java
+++ src/COMP1010/main/Character.java
@@ -74,3 +74,17 @@ class StatMod {
         this.valueOfMod = valueOfMod;
     }
 }
+class Returnpair {
+    public String name;
+    public int num;
+    public StatMod stat1;
+
+    public Returnpair(String name, int num) {
+        this.name = name;
+        this.num = num;
+    }
+    public Returnpair(String name, StatMod stat1) {
+        this.name = name;
+        this.stat1 = stat1;
+    }
+}
