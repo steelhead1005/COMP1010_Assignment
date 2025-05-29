4b227c9a6bc0ed7b28d150f2e6a9a001bcea0214
JaiveerRanawat <71818368+JaiveerRanawat@users.noreply.github.com>
2025-05-29 12:55:15 +1000

    Update Client.java
    comments

--- src/COMP1010/main/Client.java
+++ src/COMP1010/main/Client.java
@@ -2,7 +2,7 @@ package COMP1010.main;
 
 public class Client {
     public static void main(String[] args) throws Exception {
-        GameLogic.gameStart();
+        GameLogic.gameStart();//Calls the game start to intitiate the game 
     }
 }
 
